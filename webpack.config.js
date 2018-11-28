11ce3299cbaf1953f61e99e12e0b93107565ef5e
Marco Biedermann <marco.biedermann@gmx.de>
2018-11-28 08:45:42 +0100

    chore: Webpack
    Setup webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: [
+      path.resolve(__dirname, 'client/assets/js/index')
+    ]
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'server/public'),
+  }
+}
---
+++ webpack.development.config.js
@@ -0,0 +1,6 @@
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('./webpack.config');
+
+module.exports = webpackMerge(baseConfig, {
+  mode: 'development',
+});
