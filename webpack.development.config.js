26a69f71242b5d7e4b0e75b6115df58b4719ee50
Marco Biedermann <marco.biedermann@gmx.de>
2018-11-28 08:59:45 +0100

    chore: HTML
    Add HtmlWebpackPlugin

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,7 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const baseConfig = require('./webpack.config');
 
 module.exports = webpackMerge(baseConfig, {
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+    }),
+  ],
 });
