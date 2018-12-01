8a16a6fcdab48ee76d4969864832bf4d5a9eaefd
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-01 14:38:56 +0100

    feat: config
    Set configs via ENV variables

---
+++ client/assets/js/constants/open-weather-map.js
@@ -0,0 +1,2 @@
+export const OPEN_WEATHER_MAP_API_BASE = process.env.OPEN_WEATHER_MAP_API_BASE;
+export const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+
+  return prev;
+}, {})
 
 module.exports = {
   entry: {
@@ -23,6 +33,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin(envKeys),
+  ],
   resolve: {
     extensions: [
       '.js',
