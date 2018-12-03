a94d48d455ad922a273a6ca5ddb25b61b741e877
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-03 23:46:46 +0100

    feat: Current
    Add Current component

---
+++ client/assets/js/components/Current/index.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Weather from '../Weather';
+
+const Current = (props) => {
+  const { name, ...otherProps } = props;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <Weather {...otherProps} />
+    </div>
+  );
+};
+
+Current.propTypes = {
+  name: PropTypes.string,
+};
+
+Current.defaultProps = {
+  name: '',
+};
+
+export default Current;
