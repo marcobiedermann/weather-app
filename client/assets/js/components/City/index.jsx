bcdc14ebf619a3a18f4d5c35cf7e899075027b7e
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-05 09:45:19 +0100

    feat: City link
    Link to city detail page

--- client/assets/js/components/City/index.jsx
+++ client/assets/js/components/City/index.jsx
@@ -1,22 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const City = (props) => {
-  const { name } = props;
+  const { id, name } = props;
 
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>
+        <Link to={`/cities/${id}`}>{name}</Link>
+      </h2>
     </div>
   );
 };
 
 City.propTypes = {
   name: PropTypes.string,
+  id: PropTypes.number,
 };
 
 City.defaultProps = {
   name: '',
+  id: 0,
 };
 
 export default City;
