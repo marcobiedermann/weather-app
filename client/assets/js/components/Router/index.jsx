f653ca902b0f5eb3f226011ae0ce0487e40caf44
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-06 08:40:47 +0100

    feat: Layout
    Add Layout component

--- client/assets/js/components/Router/index.jsx
+++ client/assets/js/components/Router/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from '../Layout';
 import CitiesPage from '../../pages/Cities';
 import IndexPage from '../../pages/Index';
 import SettingsPage from '../../pages/Settings';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/cities" component={CitiesPage} />
-      <Route path="/settings" component={SettingsPage} />
-      <Route path="/" component={IndexPage} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/cities" component={CitiesPage} />
+        <Route path="/settings" component={SettingsPage} />
+        <Route path="/" component={IndexPage} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
