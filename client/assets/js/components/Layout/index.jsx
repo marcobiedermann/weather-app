ab416e964a7a7620e0ca7af8a65feb59122f3971
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-06 08:45:32 +0100

    feat: Main
    Add Main component

--- client/assets/js/components/Layout/index.jsx
+++ client/assets/js/components/Layout/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
+import Main from '../Main';
 
 const Layout = (props) => {
   const { children } = props;
@@ -11,7 +12,9 @@ const Layout = (props) => {
       <Header>
         HEADER
       </Header>
-      {children}
+      <Main>
+        {children}
+      </Main>
       <Footer>
         FOOTER
       </Footer>
