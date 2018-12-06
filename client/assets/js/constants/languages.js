839a4a3c96be949c296cffe0f9a82da57d49e5d1
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-06 14:28:13 +0100

    feat: Settings
    Add form to settings page

---
+++ client/assets/js/constants/languages.js
@@ -0,0 +1,134 @@
+export const languages = [
+  {
+    id: 'ar',
+    name: 'Arabic',
+  },
+  {
+    id: 'bg',
+    name: 'Bulgarian',
+  },
+  {
+    id: 'ca',
+    name: 'Catalan',
+  },
+  {
+    id: 'zh',
+    name: 'Chinese Simplified',
+  },
+  {
+    id: 'hr',
+    name: 'Croatian',
+  },
+  {
+    id: 'cz',
+    name: 'Czech',
+  },
+  {
+    id: 'nl',
+    name: 'Dutch',
+  },
+  {
+    id: 'en',
+    name: 'English',
+  },
+  {
+    id: 'fi',
+    name: 'Finnish',
+  },
+  {
+    id: 'fr',
+    name: 'French',
+  },
+  {
+    id: 'de',
+    name: 'German',
+  },
+  {
+    id: 'el',
+    name: 'Greek',
+  },
+  {
+    id: 'gl',
+    name: 'Galician',
+  },
+  {
+    id: 'hu',
+    name: 'Hungarian',
+  },
+  {
+    id: 'it',
+    name: 'Italian',
+  },
+  {
+    id: 'ja',
+    name: 'Japanese',
+  },
+  {
+    id: 'kr',
+    name: 'Korean',
+  },
+  {
+    id: 'la',
+    name: 'Latvian',
+  },
+  {
+    id: 'lt',
+    name: 'Lithuanian',
+  },
+  {
+    id: 'mk',
+    name: 'Macedonian',
+  },
+  {
+    id: 'fa',
+    name: 'Persian (Farsi)',
+  },
+  {
+    id: 'pl',
+    name: 'Polish',
+  },
+  {
+    id: 'pt',
+    name: 'Portuguese',
+  },
+  {
+    id: 'ro',
+    name: 'Romanian',
+  },
+  {
+    id: 'ru',
+    name: 'Russian',
+  },
+  {
+    id: 'sk',
+    name: 'Slovak',
+  },
+  {
+    id: 'sl',
+    name: 'Slovenian',
+  },
+  {
+    id: 'es',
+    name: 'Spanish',
+  },
+  {
+    id: 'se',
+    name: 'Swedish',
+  },
+  {
+    id: 'tr',
+    name: 'Turkish',
+  },
+  {
+    id: 'ua',
+    name: 'Ukrainian',
+  },
+  {
+    id: 'vi',
+    name: 'Vietnamese',
+  },
+];
+
+export const units = [
+
+];
--- client/assets/js/pages/Settings/index.jsx
+++ client/assets/js/pages/Settings/index.jsx
@@ -1,9 +1,45 @@
+import { Field, Form, withFormik } from 'formik';
+import PropTypes from 'prop-types';
 import React from 'react';
+import { languages } from '../../constants/languages';
 
-const SettingsPage = () => (
-  <div>
-    Settings Page
-  </div>
-);
+const SettingsPage = (props) => {
+  const { isSubmitting } = props;
 
-export default SettingsPage;
+  return (
+    <div>
+      Settings Page
+      <Form>
+        <Field component="select" name="language">
+          {languages.map(language => (
+            <option value={language.id} key={language.id}>
+              {language.name}
+            </option>
+          ))}
+        </Field>
+        <div>
+          <button type="submit" disabled={isSubmitting}>
+            Save
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+SettingsPage.propTypes = {
+  isSubmitting: PropTypes.bool,
+};
+
+SettingsPage.defaultProps = {
+  isSubmitting: false,
+};
+
+export default withFormik({
+  handleSubmit: (values) => {
+    console.log(values);
+  },
+  mapPropsToValues: () => ({
+    language: 'en',
+  }),
+})(SettingsPage);
