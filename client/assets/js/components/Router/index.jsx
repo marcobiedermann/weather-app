0af8d3108dd352151c29f37d8392bbb7f82188ee
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-06 08:33:43 +0100

    feat: Settings
    Add Settings page

--- client/assets/js/components/Router/index.jsx
+++ client/assets/js/components/Router/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CitiesPage from '../../pages/Cities';
 import IndexPage from '../../pages/Index';
+import SettingsPage from '../../pages/Settings';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/cities" component={CitiesPage} />
+      <Route path="/settings" component={SettingsPage} />
       <Route path="/" component={IndexPage} />
     </Switch>
   </BrowserRouter>
