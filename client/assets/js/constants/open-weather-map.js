2a6b4903ea4712e26b328da7db7ff4caccb3feeb
Marco Biedermann <marco.biedermann@gmx.de>
2019-01-28 08:47:13 +0100

    refactor: constants
    Shorten Open Weather API constants by removing unnessesary context

--- client/assets/js/constants/open-weather-map.js
+++ client/assets/js/constants/open-weather-map.js
@@ -1,3 +1,3 @@
 /* eslint-disable prefer-destructuring */
-export const OPEN_WEATHER_MAP_API_BASE = process.env.OPEN_WEATHER_MAP_API_BASE;
-export const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
+export const API_BASE = process.env.OPEN_WEATHER_MAP_API_BASE;
+export const API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
