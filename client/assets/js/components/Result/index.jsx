d694f9dacd35ba671a5a44d0b67d2782f92677eb
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-01 13:11:03 +0100

    feat: result
    add result component

---
+++ client/assets/js/components/Result/index.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Result = (props) => {
+  const {
+    addCity, main, name, sys,
+  } = props;
+
+  return (
+    <div>
+      <h3>
+        {name}
+        ,
+        {' '}
+        {sys.country}
+      </h3>
+      {main.temp}
+
+      <button type="button" onClick={() => addCity(props)}>Add</button>
+    </div>
+  );
+};
+
+Result.propTypes = {
+  addCity: PropTypes.func,
+  main: PropTypes.shape(),
+  name: PropTypes.string,
+  sys: PropTypes.shape(),
+};
+
+Result.defaultProps = {
+  addCity: () => {},
+  main: null,
+  name: '',
+  sys: null,
+};
+
+export default Result;
