31630614b5eb5ef09c09277e9633ee261812fbc8
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 17:10:28 +0100

    feat: data mapping
    connect city page to store

--- client/assets/js/pages/Cities/City/index.jsx
+++ client/assets/js/pages/Cities/City/index.jsx
@@ -1,34 +1,64 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
 import ForecastPage from './Forecast';
+import { fetchWeatherById } from '../../../actions/weather';
+import Weather from '../../../components/Weather';
 
-const CityPage = (props) => {
-  const { match } = props;
+class CityPage extends Component {
+  componentDidMount() {
+    const { fetchWeatherById, match } = this.props;
 
-  return (
-    <Switch>
-      <Route path={`${match.path}/forecast`} component={ForecastPage} />
-      <Route
-        path={match.path}
-        render={() => (
-          <div>
-            City Page
-          </div>
-        )}
-      />
-    </Switch>
-  );
-};
+    fetchWeatherById(match.params.cityId);
+  }
+
+  render() {
+    const { match, weather } = this.props;
+
+    return (
+      <Switch>
+        <Route path={`${match.path}/forecast`} component={ForecastPage} />
+        <Route
+          path={match.path}
+          render={() => (
+            <div>
+              <Weather {...weather} />
+            </div>
+          )}
+        />
+      </Switch>
+    );
+  }
+}
 
 CityPage.propTypes = {
+  fetchWeatherById: PropTypes.func,
   match: PropTypes.shape({
     path: PropTypes.string,
   }),
+  weather: PropTypes.shape(),
 };
 
 CityPage.defaultProps = {
+  fetchWeatherById: () => {},
   match: null,
+  weather: null,
 };
 
-export default CityPage;
+const mapStateToProps = state => ({
+  weather: state.weather,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    fetchWeatherById,
+  },
+  dispatch,
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CityPage);
