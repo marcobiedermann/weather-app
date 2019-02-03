b308ab4af94ff360ba587015419450f6c19ceb2d
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 17:09:26 +0100

    feat: reducers
    add reducers

--- client/assets/js/reducers/index.js
+++ client/assets/js/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
+import cities from './cities';
+import search from './search';
+import settings from './settings';
+import weather from './weather';
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  cities,
+  search,
+  settings,
+  weather,
+});
 
 export default reducers;
