5e2d5a7e381afefcda86179a3fe6deda109c6279
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 17:08:01 +0100

    feat: action-types
    add action-types constants

---
+++ client/assets/js/constants/action-types.js
@@ -0,0 +1,8 @@
+export const CITY_ADD = 'CITY_ADD';
+export const CITY_REMOVE = 'CITY_REMOVE';
+
+export const SEARCH_FETCH = 'SEARCH_FETCH';
+
+export const SETTINGS_UPDATE = 'SETTINGS_UPDATE';
+
+export const WEATHER_FETCH = 'WEATHER_FETCH';
