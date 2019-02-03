c3b3e346accee7b83d2525e1bf023013c39abcd7
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 21:55:05 +0100

    feat: city
    add styles to city component

--- client/assets/js/components/City/index.jsx
+++ client/assets/js/components/City/index.jsx
@@ -1,25 +1,43 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './style.module.css';
 
 const City = (props) => {
-  const { id, name } = props;
+  const {
+    className, id, main, name,
+  } = props;
 
   return (
-    <div>
-      <h2>
-        <Link to={`/cities/${id}`}>{name}</Link>
-      </h2>
+    <div
+      className={classNames(className, styles.city, {
+        [styles['city--cold']]: (main.temp <= 0),
+        [styles['city--warm']]: (main.temp >= 15),
+      })}
+    >
+      <Link to={`/cities/${id}`}>
+        {main.temp}
+        {' '}
+        °C
+        <h2>
+          {name}
+        </h2>
+      </Link>
     </div>
   );
 };
 
 City.propTypes = {
+  className: PropTypes.string,
+  main: PropTypes.shape(),
   name: PropTypes.string,
   id: PropTypes.number,
 };
 
 City.defaultProps = {
+  className: '',
+  main: null,
   name: '',
   id: 0,
 };
