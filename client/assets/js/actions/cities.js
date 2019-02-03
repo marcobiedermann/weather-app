68b906a3fdb84ebe3adcc4d62d9874262fcf9a9e
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 17:07:12 +0100

    feat: cities
    add cities actions and reducer

---
+++ client/assets/js/actions/cities.js
@@ -0,0 +1,14 @@
+import {
+  CITY_ADD,
+  CITY_REMOVE,
+} from '../constants/action-types';
+
+export const addCity = city => ({
+  type: CITY_ADD,
+  payload: city,
+});
+
+export const removeCity = id => ({
+  type: CITY_REMOVE,
+  payload: id,
+});
---
+++ client/assets/js/reducers/cities.js
@@ -0,0 +1,21 @@
+import {
+  CITY_ADD,
+  CITY_REMOVE,
+} from '../constants/action-types';
+
+const initialState = [];
+
+function citiesReducer(state = initialState, action) {
+  switch (action.type) {
+    case CITY_ADD:
+      return state.filter(city => city.id !== action.payload.id).concat([action.payload]);
+
+    case CITY_REMOVE:
+      return state.filter(city => city.id !== action.payload);
+
+    default:
+      return state;
+  }
+}
+
+export default citiesReducer;
