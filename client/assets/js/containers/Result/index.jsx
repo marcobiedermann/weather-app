832c3124f5d171c6d8a861ee5b53bfd52e4f6d84
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-03 17:05:27 +0100

    feat: result container
    add result container

---
+++ client/assets/js/containers/Result/index.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCity } from '../../actions/cities';
+import Result from '../../components/Result';
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    addCity,
+  },
+  dispatch,
+);
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Result);
