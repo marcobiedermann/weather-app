9ef86302e5cc63a54c4a1a33ac33a2d15a06db50
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-04 20:41:49 +0100

    feat: search page
    link to search page from edit page

--- client/assets/js/pages/Cities/Edit/index.jsx
+++ client/assets/js/pages/Cities/Edit/index.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { removeCity } from '../../../actions/cities';
 import City from '../../../components/City';
+import * as routes from '../../../constants/routes';
 import styles from './style.module.css';
 
 const EditPage = (props) => {
@@ -18,6 +20,9 @@ const EditPage = (props) => {
             <button type="button" onClick={() => removeCity(city.id)}>x</button>
           </li>
         ))}
+        <li>
+          <Link to={routes.SEARCH.path}>Add</Link>
+        </li>
       </ul>
     </div>
   );
