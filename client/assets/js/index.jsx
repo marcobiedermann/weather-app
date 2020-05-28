9ba497d0efe215040f822826af9f63cf3add04f0
Marco Biedermann <marco.biedermann@gmx.de>
2020-05-28 20:02:00 +0200

    feat: Sentry
    Remove Sentry

--- client/assets/js/index.jsx
+++ client/assets/js/index.jsx
@@ -1,10 +1,7 @@
 import '@babel/polyfill';
-import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import './i18n';
-
-Sentry.init({ dsn: 'https://1fbb25d6ca084d6cad465c3054d7618e@sentry.io/1395958' });
 
 render(<Root />, document.getElementById('root'));
