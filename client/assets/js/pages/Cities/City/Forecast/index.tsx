b33f2cf7e06d2401622c668a1626ddd33643f6a7
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-13 12:31:03 +0100

    feat: Forecast
    Fetch forecast by city Id from API

--- client/assets/js/pages/Cities/City/Forecast/index.tsx
+++ client/assets/js/pages/Cities/City/Forecast/index.tsx
@@ -1,15 +1,84 @@
 import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
+import useSWR from 'swr';
+import Loader from '../../../../components/Loader';
 
 interface Params {
   cityId: string;
 }
 
+interface ForecastPageQuery {
+  city: City;
+  cnt: number;
+  cod: string;
+  list: List[];
+  message: number;
+}
+
+interface City {
+  coord: Coord;
+  country: string;
+  id: number;
+  name: string;
+  population: number;
+  timezone: number;
+}
+
+interface Coord {
+  lat: number;
+  lon: number;
+}
+
+interface List {
+  clouds: number;
+  deg: number;
+  dt: number;
+  feels_like: FeelsLike;
+  humidity: number;
+  pop: number;
+  pressure: number;
+  speed: number;
+  sunrise: number;
+  sunset: number;
+  temp: Temp;
+  weather: Weather[];
+}
+
+interface FeelsLike {
+  day: number;
+  eve: number;
+  morn: number;
+  night: number;
+}
+
+interface Temp {
+  day: number;
+  eve: number;
+  max: number;
+  min: number;
+  morn: number;
+  night: number;
+}
+
+interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
 const ForecastPage: FC = () => {
   const { cityId } = useParams<Params>();
+  const { data, error } = useSWR<ForecastPageQuery>(`/forecast/daily?id=${cityId}`);
 
-  // const { data } = fetchForecastById(cityId);
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <Loader />;
+  }
 
   return (
     <div>
@@ -17,7 +86,7 @@ const ForecastPage: FC = () => {
         <title>Forecast</title>
       </Helmet>
       <ul>
-        {forecast.list.map((item) => (
+        {data.list.map((item) => (
           <li key={item.dt}>
             <div>Min: {item.temp.min}</div>
             <div>Max: {item.temp.max}</div>
--- client/assets/js/reducers/index.ts
+++ client/assets/js/reducers/index.ts
@@ -1,14 +1,12 @@
 import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import cities from './cities';
-import forecast from './forecast';
 import search from './search';
 import settings from './settings';
 
 const reducers = (history) =>
   combineReducers({
     cities,
-    forecast,
     router: connectRouter(history),
     search,
     settings,
