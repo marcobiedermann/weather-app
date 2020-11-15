62a9b031eb58dfc84d00665e9b68c07c6f6ad463
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-15 11:51:04 +0100

    test: Fixtures
    Update fixtures containing real data

--- src/assets/js/components/Result/__fixtures__/index.ts
+++ src/assets/js/components/Result/__fixtures__/index.ts
@@ -1,11 +1,38 @@
 const result = {
-  id: 0,
-  main: {
-    temp: 0,
+  clouds: {
+    all: 0,
   },
-  name: '',
+  coord: {
+    lat: 52.5244,
+    lon: 13.4105,
+  },
+  dt: 1605434959,
+  id: 2950159,
+  main: {
+    feels_like: 9.39,
+    humidity: 76,
+    pressure: 1014,
+    temp_max: 13.89,
+    temp_min: 11,
+    temp: 12.61,
+  },
+  name: 'Berlin',
+  rain: null,
+  snow: null,
   sys: {
-    country: '',
+    country: 'DE',
+  },
+  weather: [
+    {
+      description: 'clear sky',
+      icon: '01d',
+      id: 800,
+      main: 'Clear',
+    },
+  ],
+  wind: {
+    deg: 170,
+    speed: 4.1,
   },
 };
 
