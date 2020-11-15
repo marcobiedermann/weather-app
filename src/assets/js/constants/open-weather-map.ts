dacfe1d6b0601ead4a12e5fbf3322a056a514fcc
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-15 12:36:52 +0100

    chore: JSON Server
    Add JSON Server to mock API

--- src/assets/js/constants/open-weather-map.ts
+++ src/assets/js/constants/open-weather-map.ts
@@ -1,2 +1,2 @@
-export const API_BASE = 'https://api.openweathermap.org/data/2.5';
+export const API_BASE = process.env.OPEN_WEATHER_MAP_API_BASE;
 export const API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
