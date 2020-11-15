7919062158c2d0c1f6889d578f0819ae29b8b8b1
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-15 12:48:25 +0100

    chore: Typed CSS Modules
    Generate types for CSS Modules

---
+++ src/assets/js/components/Cities/style.module.css.d.ts
@@ -0,0 +1,4 @@
+declare const styles: {
+  readonly cities: string;
+};
+export = styles;
---
+++ src/assets/js/components/CityPreview/style.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly city: string;
+  readonly 'city--cold': string;
+  readonly 'city--warm': string;
+};
+export = styles;
