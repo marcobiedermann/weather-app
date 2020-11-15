7046832031f62f96a3c06d7f92879adc14a9a23c
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-15 11:39:45 +0100

    chore: Sentry
    Add Sentry error tracking

--- src/assets/js/index.tsx
+++ src/assets/js/index.tsx
@@ -1,6 +1,14 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import './i18n';
+
+Sentry.init({
+  dsn: 'https://01859cefc3b84a959394e2f2972a2a74@o81678.ingest.sentry.io/5517125',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 render(<Root />, document.getElementById('root'));
