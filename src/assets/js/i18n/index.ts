7b8f4cbdadd6e0a5f60697cc26a538f8efc406ec
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-18 13:07:18 +0100

    fix: i18next logging
    Enable i18next loggin only in development mode

--- src/assets/js/i18n/index.ts
+++ src/assets/js/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
+import config from '../constants/config';
 
 i18n
   .use(Backend)
@@ -11,7 +12,7 @@ i18n
     backend: {
       loadPath: 'data/locales/{{lng}}/{{ns}}.json',
     },
-    debug: true,
+    debug: config.environment === 'development',
     fallbackLng: 'en',
     lng: 'de',
     interpolation: {
