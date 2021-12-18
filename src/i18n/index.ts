b3ccf3af76a1879b12fa1c3f1c40dd67516ed9bf
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2021-12-18 11:48:52 +0100

    feat: i18next Backend
    Replace i18next-xhr-backend with i18next-http-backend

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
+import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import { isDevelopment } from '../utils/environment';
 
 i18n
-  .use(Backend)
+  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
