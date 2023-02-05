5df96da8234612a7d772ae060548ec3c53a22f8a
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-02-05 20:42:27 +0100

    refactor: Mock Handlers
    Refactor MSW handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,31 +6,27 @@ import forecast from './__fixtures__/forecast/london.json';
 import group from './__fixtures__/group/berlin.json';
 import weather from './__fixtures__/weather/london.json';
 
+const BASE_PATH = 'https://api.openweathermap.org/data/2.5';
+
 const handlers = [
-  rest.get('https://api.openweathermap.org/data/2.5/find', (_request, response, context) => {
-    return response(context.status(200), context.json(find));
-  }),
-  rest.get(
-    'https://api.openweathermap.org/data/2.5/forecast/daily',
-    (_request, response, context) => {
-      return response(context.status(200), context.json(dailyForecast));
-    },
+  rest.get(`${BASE_PATH}/find`, (_request, response, context) =>
+    response(context.status(200), context.json(find)),
   ),
-  rest.get(
-    'https://api.openweathermap.org/data/2.5/forecast/hourly',
-    (_request, response, context) => {
-      return response(context.status(200), context.json(hourlyForecast));
-    },
+  rest.get(`${BASE_PATH}/forecast/daily`, (_request, response, context) =>
+    response(context.status(200), context.json(dailyForecast)),
   ),
-  rest.get('https://api.openweathermap.org/data/2.5/forecast', (_request, response, context) => {
-    return response(context.status(200), context.json(forecast));
-  }),
-  rest.get('https://api.openweathermap.org/data/2.5/group', (_request, response, context) => {
-    return response(context.status(200), context.json(group));
-  }),
-  rest.get('https://api.openweathermap.org/data/2.5/weather', (_request, response, context) => {
-    return response(context.status(200), context.json(weather));
-  }),
+  rest.get(`${BASE_PATH}/forecast/hourly`, (_request, response, context) =>
+    response(context.status(200), context.json(hourlyForecast)),
+  ),
+  rest.get(`${BASE_PATH}/forecast`, (_request, response, context) =>
+    response(context.status(200), context.json(forecast)),
+  ),
+  rest.get(`${BASE_PATH}/group`, (_request, response, context) =>
+    response(context.status(200), context.json(group)),
+  ),
+  rest.get(`${BASE_PATH}/weather`, (_request, response, context) =>
+    response(context.status(200), context.json(weather)),
+  ),
 ];
 
 export { handlers };
