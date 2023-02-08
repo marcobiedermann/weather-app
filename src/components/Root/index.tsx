7723fa5265308d92352d0263576350109b9dee3d
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-02-08 10:00:32 +0100

    feat: Persistor
    Persist Redux state in Localstorage

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from '../../store';
 import Loader from '../Loader';
 import Providers from '../Providers';
 import Router from '../Router';
@@ -9,11 +11,13 @@ const queryClient = new QueryClient();
 function Root(): JSX.Element {
   return (
     <Providers>
-      <Suspense fallback={<Loader />}>
-        <QueryClientProvider client={queryClient}>
-          <Router />
-        </QueryClientProvider>
-      </Suspense>
+      <PersistGate loading={null} persistor={persistor}>
+        <Suspense fallback={<Loader />}>
+          <QueryClientProvider client={queryClient}>
+            <Router />
+          </QueryClientProvider>
+        </Suspense>
+      </PersistGate>
     </Providers>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,30 @@
 /* eslint-disable import/no-cycle */
 
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import cities from '../reducers/cities';
 import settings from '../reducers/settings';
 
-const store = configureStore({
-  reducer: {
-    cities,
-    settings,
-  },
+const rootReducer = combineReducers({
+  cities,
+  settings,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
+export { persistor };
 export default store;
