19ac96b3237defe8ba9b97ccffe970539f4fa93f
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-06-06 16:53:20 +0200

    test: Vitest
    Migrate from Jest to Vitest

--- src/components/City/__tests__/index.test.tsx
+++ src/components/City/__tests__/index.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { describe, expect, it } from 'vitest';
 import City from '..';
 import cityFixtures from '../__fixtures__';
 
--- src/hooks/index.test.tsx
+++ src/hooks/index.test.tsx
@@ -1,10 +1,11 @@
 import { renderHook, waitFor } from '@testing-library/react';
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
 import { useFind } from '.';
 import Providers from '../components/Providers';
 import { server } from '../mocks/server';
 import berlin from '../mocks/__fixtures__/find/berlin.json';
 
-jest.mock('../constants/open-weather-map');
+vi.mock('../constants/open-weather-map');
 
 describe('hooks/index', () => {
   beforeAll(() => server.listen());
