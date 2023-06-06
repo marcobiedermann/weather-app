f4b4323c430738b1d6f3943bc6e0569e44b8b22f
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-06-06 15:25:36 +0200

    chore: Dependencies
    Update Node dependencies

--- src/components/Results/__tests__/index.test.tsx
+++ src/components/Results/__tests__/index.test.tsx
@@ -3,13 +3,6 @@ import React from 'react';
 import Results from '..';
 import resultsFixtures from '../__fixtures__';
 
-const mockDispatch = jest.fn();
-
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(),
-  useDispatch: () => mockDispatch,
-}));
-
 describe('components/Results', () => {
   it('renders correctly', () => {
     expect.assertions(1);
