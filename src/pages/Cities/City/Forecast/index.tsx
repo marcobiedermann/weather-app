35cff4ee0cc2bd5576defe21df172e143b7cd129
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-06-07 17:02:48 +0200

    feat: Validation
    Validate user controled input via `zod`

--- src/pages/Cities/City/Forecast/index.tsx
+++ src/pages/Cities/City/Forecast/index.tsx
@@ -5,10 +5,12 @@ import Error from '../../../../components/Error';
 import Forecast from '../../../../components/Forecast';
 import Loader from '../../../../components/Loader';
 import { useDailyForeCast } from '../../../../hooks';
+import { paramsSchema } from '..';
 
 function ForecastPage(): JSX.Element {
-  const { cityId } = useParams();
-  const { data, error, isError, isLoading } = useDailyForeCast(parseInt(cityId!, 10));
+  const params = useParams();
+  const { cityId } = paramsSchema.parse(params);
+  const { data, error, isError, isLoading } = useDailyForeCast(cityId);
 
   if (isError) {
     return <Error message={error.message} />;
