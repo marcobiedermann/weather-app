0eb2fb91fdfd3691057b9eaae846e0bdd05376cf
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-06-09 17:24:50 +0200

    fix: Coverage
    Collect coverage from all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'v8',
+      all: true,
+    },
     css: {
       modules: {
         classNameStrategy: 'non-scoped',
