cb9dc678512d860ce1ade6dcc564eeb5809a7a76
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-11 15:02:30 +0200

    chore: `msw`
    Upgrade `msw` to `v2`

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,4 +1,4 @@
-import { rest } from 'msw';
+import { HttpResponse, http } from 'msw';
 import find from './__fixtures__/find/berlin.json';
 import forecast from './__fixtures__/forecast/berlin.json';
 import dailyForecast from './__fixtures__/forecast/daily/berlin.json';
@@ -8,21 +8,11 @@ import weather from './__fixtures__/weather/berlin.json';
 const BASE_PATH = 'https://api.openweathermap.org/data/2.5';
 
 const handlers = [
-  rest.get(`${BASE_PATH}/find`, (_request, response, context) =>
-    response(context.status(200), context.json(find)),
-  ),
-  rest.get(`${BASE_PATH}/forecast`, (_request, response, context) =>
-    response(context.status(200), context.json(forecast)),
-  ),
-  rest.get(`${BASE_PATH}/forecast/daily`, (_request, response, context) =>
-    response(context.status(200), context.json(dailyForecast)),
-  ),
-  rest.get(`${BASE_PATH}/group`, (_request, response, context) =>
-    response(context.status(200), context.json(group)),
-  ),
-  rest.get(`${BASE_PATH}/weather`, (_request, response, context) =>
-    response(context.status(200), context.json(weather)),
-  ),
+  http.get(`${BASE_PATH}/find`, () => HttpResponse.json(find)),
+  http.get(`${BASE_PATH}/forecast`, () => HttpResponse.json(forecast)),
+  http.get(`${BASE_PATH}/forecast/daily`, () => HttpResponse.json(dailyForecast)),
+  http.get(`${BASE_PATH}/group`, () => HttpResponse.json(group)),
+  http.get(`${BASE_PATH}/weather`, () => HttpResponse.json(weather)),
 ];
 
 export { handlers };
